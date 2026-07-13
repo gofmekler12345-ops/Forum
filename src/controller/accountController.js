dae679014931b0753de8902d83bf7bdfdc9a5963
PC <gofmekler12345@gmail.com>
2026-07-13 18:27:25 +0300

    13/07/2026

--- src/controller/accountController.js
+++ src/controller/accountController.js
@@ -13,39 +13,39 @@ export const login = async (req, res) => {
 
 }
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res, next) => {
     try {
         const user = await service.deleteUser(req.params.user);
         return res.json(user);
     } catch (e) {
-        return e;
+        return next(e);
     }
 }
 
-export const updateUser = async (req, res) => {
+export const updateUser = async (req, res, next) => {
     try {
         const user = await service.updateUser(req.params.user, req.body);
         return res.json(user);
     } catch (e) {
-        return e;
+        return next(e);
     }
 }
 
-export const addRole = async (req, res) => {
+export const addRole = async (req, res, next) => {
     try {
         const user = await service.addRole(req.params.user, req.params.role);
         return res.json(user);
     } catch (e) {
-        return e;
+        return next(e);
     }
 }
 
-export const deleteRole = async (req, res) => {
+export const deleteRole = async (req, res, next) => {
     try {
         const user = await service.deleteRole(req.params.user, req.params.role);
         return res.json(user);
     } catch (e) {
-        return e;
+        return next(e);
     }
 }
 
@@ -53,11 +53,11 @@ export const changePassword = async (req, res) =>{
 
 }
 
-export const getUser = async (req, res) => {
+export const getUser = async (req, res, next) => {
     try {
         const user = await service.getUser(req.params.user);
         return res.json(user);
     } catch (e) {
-        return e;
+        return next(e);
     }
 }
