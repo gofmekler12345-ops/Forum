818a66c9190335fee760da9c025ba54c99449749
PC <gofmekler12345@gmail.com>
2026-07-23 16:26:55 +0300

    23/07/2026

--- src/routes/authorizationRoutes.js
+++ src/routes/authorizationRoutes.js
@@ -3,9 +3,9 @@ import {hasRole, isOwnerOrHasRole, isOwner, isLoginIsAuthor} from "../middleware
 import {ADMIN} from "../configuretion/constant.js";
 
 const router = Router();
-router.all('/account/user/:login/role/:role', hasRole(ADMIN))
-router.all('/account/user/:login', isOwner('login'))
-router.all('/account/user/:login', isOwnerOrHasRole('login'))
-router.all('/post/:author', isLoginIsAuthor('author'))
-router.all('/post/:id/comment/:author', isLoginIsAuthor('author'))
+router.all('/account/user/:login/role/:role', hasRole(ADMIN));
+router.patch('/account/user/:user', isOwner('user'));
+router.delete('/account/user/:login', isOwnerOrHasRole('login', ADMIN));
+router.post('/post/:author', isLoginIsAuthor('author'))
+router.patch('/post/:id/comment/:author', isLoginIsAuthor('author'))
 export default router;
